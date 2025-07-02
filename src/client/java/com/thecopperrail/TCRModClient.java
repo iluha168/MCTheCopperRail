b3d8dba15c19ee5fbdc84ea27b8e58f3228943da
iluha168 <illya.chernyshov888@gmail.com>
2025-07-02 22:08:11 +0700

    0.9.7-c
    - Bump to minecraft 1.21.6

--- src/client/java/com/thecopperrail/TCRModClient.java
+++ src/client/java/com/thecopperrail/TCRModClient.java
@@ -1,12 +1,12 @@
 package com.thecopperrail;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class TCRModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		BlockRenderLayerMap.INSTANCE.putBlock(TCRMod.BLOCK, RenderLayer.getCutout());
+		BlockRenderLayerMap.putBlock(TCRMod.BLOCK, BlockRenderLayer.CUTOUT);
 	}	
 }
